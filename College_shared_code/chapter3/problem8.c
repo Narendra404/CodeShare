84d23b50bb4908bc32f0daaa61e2b9c2a9141bda
Narendra404 <npaul7345@gmail.com>
2021-07-03 13:17:11 +0000

    Add solutions for problems 2 to 9 of chapter 3

---
+++ College_shared_code/chapter3/problem8.c
@@ -0,0 +1,27 @@
+/*
+Write a program to find the absolute value of a number entered 
+through the keyboard.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("Enter a number (positive, negative or zero) : \n");
+    scanf("%d", &n);
+
+    if (n > 0)
+    {
+        printf("The absolute value of %d is %d.\n", n, n);
+    }
+    else if (n < 0)
+    {
+        printf("The absolute value of %d is %d.\n", n, - n);
+    }
+    else 
+    {
+        printf("The absolute value of %d is zero(%d).\n", n, n);
+    }
+
+    return 0;
+}
