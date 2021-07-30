b55990f67a5126b526105a902a6c6535fd9152f9
Narendra404 <npaul7345@gmail.com>
2021-07-30 19:27:25 +0530

    Add solution to problem 01 of Exercise2 of chapter4

---
+++ College_shared_code/LetUsC/chapter4/Exercise2/problem01.c
@@ -0,0 +1,38 @@
+/*
+Using conditional operators determine:
+(1) Whether the characters entered through the keyboard is a 
+lower case alphabet or not.
+(2)Whether a character entered through the keyboard is a special
+ symbol or not.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int p;
+    printf("Enter a character: \n");
+    scanf("%c", &ch);
+
+    p = (int)ch;
+
+    if ((p >= 97) && (p <= 122))
+    {
+        printf("The character is a lower case alphabet.\n");
+    }
+    else
+    {
+        printf("The character is not a lower case alphabet.\n");
+    }
+    if (((p >= 0) && (p <= 47)) || ((p >= 58) && (p <= 64)) || ((p >= 91) && (p <= 96)) || ((p >= 123) && (p <= 127)))
+    {
+        printf("The character is a special symbol.\n");
+    }
+    else
+    {
+        printf("The character is not a special symbol.\n");
+    }
+    
+    return 0;
+}
