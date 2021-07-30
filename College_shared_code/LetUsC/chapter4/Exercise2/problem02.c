95aa4321e1b763c71709c76280304468dd06bb82
Narendra404 <npaul7345@gmail.com>
2021-07-30 22:55:35 +0530

    Add solution to problem 02 of Exercise 2 of chapter 4

---
+++ College_shared_code/LetUsC/chapter4/Exercise2/problem02.c
@@ -0,0 +1,34 @@
+/*
+Write a program using conditional operators to determine whether 
+a year entered through the keyboard is a leap year or not.
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("Enter a year : \n");
+    scanf("%d", &n);
+
+    if ((n % 4 == 0) && (n % 100 != 0)) 
+    {
+        printf("It is a leap year.\n");
+    }
+    else if ((n % 4 == 0) && (n % 100 == 0))
+    {
+        if (n % 400 == 0)
+        {
+            printf("It is a leap year.\n");
+        }
+        else
+        {
+            printf("It is not a leap year.\n");
+        }
+    }
+    else
+    {
+            printf("It is not a leap year.\n");
+    }
+    
+    return 0;
+}
