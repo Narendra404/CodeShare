60e195a498779e93771d5414673c37f69341d5ae
Narendra404 <npaul7345@gmail.com>
2021-07-31 14:06:04 +0530

    Add solution to problem 05 of Exercise2 of chapter4

---
+++ College_shared_code/LetUsC/chapter4/Exercise2/problem05.c
@@ -0,0 +1,38 @@
+/*
+Rewrite the following program using conditional operators:
+
+#include <stdio.h>
+int main()
+{
+    float sal;
+
+    printf("Enter the salary");
+    scanf("%f", &sal);
+    if (sal >= 25000 && sal <= 40000)
+        printf("Manager\n");
+    else
+        if(sal >= 15000 && sal < 25000)
+            printf("Accountant\n");
+        else
+             printf("Clerk\n");
+    return 0;       
+}
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    float sal;
+
+    printf("Enter the salary");
+    scanf("%f", &sal);
+    (sal >= 25000 && sal <= 40000) ?
+        printf("Manager\n")
+    :
+        (sal >= 15000 && sal < 25000) ?
+            printf("Accountant\n")
+        :
+             printf("Clerk\n");
+    return 0;       
+
+}
