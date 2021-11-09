cea246e5a3af7c7e7cd9a1896adc7edb5b1463b8
Narendra404 <npaul7345@gmail.com>
2021-11-09 05:42:05 +0000

    Formatted BinarySearch.c correctly

--- College_shared_code/DSA/BinarySearch.c
+++ College_shared_code/DSA/BinarySearch.c
@@ -1,32 +1,48 @@
 #include<stdio.h>
-#include<conio.h>
 
-void main()
+int main()
 {
-   int first, last, middle, size, i, sElement, list[100];
-   clrscr();
+    int first, last, middle, size, i, sElement, list[100];
+
     printf("Enter the size of the list: ");
-   scanf("%d",&size);
-   printf("Enter %d integer values in Assending order\n", size);
-   for (i = 0; i < size; i++)
-      scanf("%d",&list[i]);
-   printf("Enter value to be search: ");
-   scanf("%d", &sElement);
-   first = 0;
-   last = size - 1;
-   middle = (first+last)/2;
-   while (first <= last) {
-      if (list[middle] < sElement)
-         first = middle + 1;    
-      else if (list[middle] == sElement) {
-         printf("Element found at index %d.\n",middle);
-         break;
-      }
-      else
-         last = middle - 1;
+    scanf("%d",&size);
+
+    printf("Enter %d integer values in Assending order\n", size);
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d",&list[i]);
+    }
+
+    printf("Enter value to be search: ");
+    scanf("%d", &sElement);
+
+    first = 0;
+    last = size - 1;
+    middle = (first+last)/2;
+
+    while (first <= last) 
+    {
+        if (list[middle] < sElement)
+        {
+            first = middle + 1;    
+        }
+        else if (list[middle] == sElement) 
+        {
+            printf("Element found at index %d.\n",middle);
+            break;
+        }
+        else
+        {
+            last = middle - 1;
+        }
+
         middle = (first + last)/2;
-   }
-  if (first > last)
-  printf("Element Not found in the list.");
-   getch();  
+    }
+
+    if (first > last)
+    {
+        printf("Element Not found in the list.");
+    }
+
+    return 0;
 }
