fc317fcb5bc24055af7ebeb222a501b7d86f7f76
Narendra404 <npaul7345@gmail.com>
2022-02-03 22:08:34 +0530

    Delete all programs under PythonPrograms and the folder itself

--- College_shared_code/PythonPrograms/CollegeProgram01.py
+++
@@ -1,8 +0,0 @@
-#WAP to check whether a number input by user is Even or Odd.
-
-num = int (input("Enter a number : "))
-
-if num % 2 == 0 :
-	print("The number is Even.")
-else :
-	print("The number is Odd.")
--- College_shared_code/PythonPrograms/CollegeProgram05.py
+++
@@ -1,3 +0,0 @@
-s = input("Enter a string : ")
-l = len(s)
-print(f"The length of the string is {l}.")
