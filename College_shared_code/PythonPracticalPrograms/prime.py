f918eac491b2964cfddb7a18e85be286fe8c3e44
Narendra404 <npaul7345@gmail.com>
2022-02-04 15:44:42 +0530

    Add solution of prime problem

---
+++ College_shared_code/PythonPracticalPrograms/prime.py
@@ -0,0 +1,24 @@
+# program to find first n prime numbers
+
+# function to find if a number is prime or not
+def isprime(n):
+    for i in range(2, n):
+        if (n % i == 0):
+            return False
+    return True
+
+# variable to store the number of prime numbers
+num=int(input("Enter range:"))
+
+print("Prime numbers:",end=' ')
+
+# some loop variables initialized
+i = 0
+n = 2
+
+# loop to print the prime numbers
+while (i < num):
+    if (isprime(n) == True) :
+        i += 1
+        print(n,end=' ')
+    n += 1
