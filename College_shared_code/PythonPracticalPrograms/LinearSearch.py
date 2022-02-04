7af0783cdd43f572167433eecb919584c07a4b75
Narendra404 <npaul7345@gmail.com>
2022-02-04 09:51:43 +0530

    modify Linear Search problem

--- College_shared_code/PythonPracticalPrograms/LinearSearch.py
+++ College_shared_code/PythonPracticalPrograms/LinearSearch.py
@@ -1,25 +1,33 @@
+# A program to implement Linear Search
+
+# declare an empyt list
 arr = []
-ch = 'y'
-while(ch == 'y'):
 
-    n = int(input("Enter the size of list : "))
+# variable to store size of list
+n = int(input("Enter the size of list : "))
 
-    for i in range((n)):
-        a = int(input(f"Enter the {i + 1}th element : "))
-        arr.append(a)
+# enter elements in the list
+for i in range((n)):
+    a = int(input(f"Enter element no. {i + 1}: "))
+    arr.append(a)
 
-    print("Elements in the list are :")
-    print(arr)
+# display the elements in the list
+print("Elements in the list are :")
+print(arr)
 
-    search = int(input("Enter the intger to be searched : "))
-    flag = 0
-    for i in range((n)):
-        if(search == arr[i]):
-            print(f"Element found at index {i}.")
-            flag = 1
-            break
+# variable to store the element to be searched
+search = int(input("Enter the intger to be searched : "))
 
-    if (flag == 0):
-        print("Element not present in the list.")
+# variable to determine whether element is found
+flag = 0
 
-    ch = input("Want to continue(y/n) : ")
+# loop to perform search
+for i in range((n)):
+    if(search == arr[i]):
+        print(f"Element found at index {i}.")
+        flag = 1
+        break
+
+# displaying appropriate message when element not found
+if (flag == 0):
+    print("Element not present in the list.")
