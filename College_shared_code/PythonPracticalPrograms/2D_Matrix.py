e5ce500076cb18c6457c38d719dfe68963a3c82b
Narendra404 <npaul7345@gmail.com>
2022-02-04 16:04:54 +0530

    modify the problem 2D Matrix

--- College_shared_code/PythonPracticalPrograms/2D_Matrix.py
+++ College_shared_code/PythonPracticalPrograms/2D_Matrix.py
@@ -1,19 +1,42 @@
-arr = []
+# Program to multiply two matrices using nested loops
+ 
+# take a 3x3 matrix
+A = [[12, 7, 3],
+    [4, 5, 6],
+    [7, 8, 9]]
+ 
+# display the elements of matrix
+print("The value of A is :")
+for i in A:
+    print(i)
+print()
 
-print("2D Matrix")
+# take a 3x4 matrix   
+B = [[5, 8, 1, 2],
+    [6, 7, 3, 0],
+    [4, 5, 9, 1]]
+     
+# display the elements of matrix
+print("The value of B is :")
+for j in B:
+    print(j)
+print()
 
-m = int(input("Enter the number of rows of the matrix : "))
-n = int(input("Enter the number of columns of the matrix : "))
-
-print("Enter the elements of the matrix one by one.")
-
-for i in range(m):
-    for j in range(n):
-        k = int(input(f"Enter the element[{i}][{j}] : "))
-        arr.append(k)
-        
-for i in range(m * n):
-    print(arr[i], end=" ")
-
-    if (((i + 1) % n) == 0):
-        print()
+result = [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]]
+ 
+# iterating by row of A
+for i in range(len(A)):
+ 
+    # iterating by column by B
+    for j in range(len(B[0])):
+ 
+        # iterating by rows of B
+        for k in range(len(B)):
+            result[i][j] += A[i][k] * B[k][j]
+ 
+# printing the result
+print("The value of A * B is :")
+for r in result:
+    print(r)
